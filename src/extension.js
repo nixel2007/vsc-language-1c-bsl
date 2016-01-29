1240f143dacff0acab35292c66227e2aa4c2c292
Nikita Gryzlov <nixel2007@gmail.com>
2016-01-29 15:40:05 +0300

    xDrivenDevelopment/1c-syntax#78 - авто отсупы. and fix xDrivenDevelopment/1c-syntax#80.
    А так же мелкие правки README

--- src/extension.js
+++ src/extension.js
@@ -1,16 +1,36 @@
 var vscode = require('vscode');
 
 var bslProviders = require('./providers');
-var bslCommands = require('./commands');
 
 function activate(context) {
-    
-    context.subscriptions.push(vscode.commands.registerCommand('extension.addpipe', bslCommands.addPipe));
+
 
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider("bsl", new bslProviders.DocumentSymbolProvider()));
 
     context.subscriptions.push(vscode.languages.registerDefinitionProvider("bsl", new bslProviders.DefinitionProvider()));
+    
+    vscode.languages.setLanguageConfiguration("bsl", {
+       
+        indentationRules: {
+            decreaseIndentPattern: /^\s*(конецесли|конеццикла|конецпроцедуры|конецфункции|иначе|иначеесли|конецпопытки|исключение|endif|enddo|endprocedure|endfunction|else|elseif|endtry|except).*$/i,
+            increaseIndentPattern: /^\s*(пока|процедура|функция|если|иначе|иначеесли|попытка|исключение|для|while|procedure|function|if|else|elseif|try|for)[^;]*$/i
+        },
+        comments: {
+            lineComment: '//'
+        },
+        onEnterRules: [
+            {
+                beforeText: /^\s*\|([^\"]|"[^\"]*")*$/,
+                action: { indentAction: vscode.IndentAction.None, appendText: '|' }
+            },
+            {
+                beforeText: /^([^\|\"]|"[^\"]*")*\"[^\"]*$/,
+                action: { indentAction: vscode.IndentAction.None, appendText: '|' }
+            }
 
+        ]
+        
+    });
 }
 
 exports.activate = activate;
