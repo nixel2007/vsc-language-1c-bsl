46767d9572c953eb2fb985313fe9b6f48b19497c
Nikita Gryzlov <nixel2007@gmail.com>
2016-01-29 17:19:47 +0300

    Fix xDrivenDevelopment/1c-syntax#79. Настройка отступов

--- src/extension.js
+++ src/extension.js
@@ -31,6 +31,35 @@ function activate(context) {
         ]
         
     });
+    
+    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(function (textEditor) {
+            applyConfigToTextEditor(textEditor);
+    }));
+    applyConfigToTextEditor(vscode.window.activeTextEditor);
+
 }
 
 exports.activate = activate;
+
+function applyConfigToTextEditor(textEditor) {
+  if (textEditor == null) {
+      return;
+  }
+  var newOptions = {
+    "insertSpaces" : false,
+    "tabSize" : 4
+  };
+  var defaultOptions = {
+    "insertSpaces" : vscode.workspace.getConfiguration("editor").get("insertSpaces"),
+    "tabSize" : vscode.workspace.getConfiguration("editor").get("tabSize")
+  };
+  if (textEditor.document.languageId == "bsl") {
+    if (textEditor.options.insertSpaces == defaultOptions.insertSpaces && (textEditor.options.tabSize == defaultOptions.tabSize || defaultOptions.tabSize == "auto")) {
+      textEditor.options = newOptions;
+    }
+  } else {
+    if (textEditor.options.insertSpaces == newOptions.insertSpaces && textEditor.options.tabSize == newOptions.tabSize) {
+      textEditor.options = defaultOptions
+    }
+  }
+}
