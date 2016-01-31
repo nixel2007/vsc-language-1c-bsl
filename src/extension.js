fe4f7ce6151cc1d5bc109bf9c685a535b5a3ff67
Nikita Gryzlov <nixel2007@gmail.com>
2016-01-31 16:19:52 +0300

    Fix indent

--- src/extension.js
+++ src/extension.js
@@ -4,39 +4,39 @@ var bslProviders = require('./providers');
 
 function activate(context) {
 
-    context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider("bsl", new bslProviders.DocumentSymbolProvider()));
+  context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider("bsl", new bslProviders.DocumentSymbolProvider()));
 
-    context.subscriptions.push(vscode.languages.registerDefinitionProvider("bsl", new bslProviders.DefinitionProvider()));
-    
-    context.subscriptions.push(vscode.languages.registerCompletionItemProvider("bsl", new bslProviders.CompletionItemProvider(), '.', '='));
+  context.subscriptions.push(vscode.languages.registerDefinitionProvider("bsl", new bslProviders.DefinitionProvider()));
 
-    vscode.languages.setLanguageConfiguration("bsl", {
-       
-        indentationRules: {
-            decreaseIndentPattern: /^\s*(конецесли|конеццикла|конецпроцедуры|конецфункции|иначе|иначеесли|конецпопытки|исключение|endif|enddo|endprocedure|endfunction|else|elseif|endtry|except).*$/i,
-            increaseIndentPattern: /^\s*(пока|процедура|функция|если|иначе|иначеесли|попытка|исключение|для|while|procedure|function|if|else|elseif|try|for)[^;]*$/i
-        },
-        comments: {
-            lineComment: '//'
-        },
-        onEnterRules: [
-            {
-                beforeText: /^\s*\|([^\"]|"[^\"]*")*$/,
-                action: { indentAction: vscode.IndentAction.None, appendText: '|' }
-            },
-            {
-                beforeText: /^([^\|\"]|"[^\"]*")*\"[^\"]*$/,
-                action: { indentAction: vscode.IndentAction.None, appendText: '|' }
-            }
+  context.subscriptions.push(vscode.languages.registerCompletionItemProvider("bsl", new bslProviders.CompletionItemProvider(), '.', '='));
 
-        ]
-        
-    });
-    
-    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(function (textEditor) {
-            applyConfigToTextEditor(textEditor);
-    }));
-    applyConfigToTextEditor(vscode.window.activeTextEditor);
+  vscode.languages.setLanguageConfiguration("bsl", {
+
+    indentationRules: {
+      decreaseIndentPattern: /^\s*(конецесли|конеццикла|конецпроцедуры|конецфункции|иначе|иначеесли|конецпопытки|исключение|endif|enddo|endprocedure|endfunction|else|elseif|endtry|except).*$/i,
+      increaseIndentPattern: /^\s*(пока|процедура|функция|если|иначе|иначеесли|попытка|исключение|для|while|procedure|function|if|else|elseif|try|for)[^;]*$/i
+    },
+    comments: {
+      lineComment: '//'
+    },
+    onEnterRules: [
+      {
+        beforeText: /^\s*\|([^\"]|"[^\"]*")*$/,
+        action: { indentAction: vscode.IndentAction.None, appendText: '|' }
+      },
+      {
+        beforeText: /^([^\|\"]|"[^\"]*")*\"[^\"]*$/,
+        action: { indentAction: vscode.IndentAction.None, appendText: '|' }
+      }
+
+    ]
+
+  });
+
+  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(function (textEditor) {
+    applyConfigToTextEditor(textEditor);
+  }));
+  applyConfigToTextEditor(vscode.window.activeTextEditor);
 
 }
 
@@ -44,7 +44,7 @@ exports.activate = activate;
 
 function applyConfigToTextEditor(textEditor) {
   if (textEditor == null) {
-      return;
+    return;
   }
   var newOptions = {
     "insertSpaces" : false,
