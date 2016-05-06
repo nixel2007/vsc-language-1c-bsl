10dde0c284173906adf68f57e36d52779f96a8b5
Nikita Gryzlov <nixel2007@gmail.com>
2016-05-06 16:04:05 +0300

    Вынесен fullNameRecursor.
    https://github.com/xDrivenDevelopment/1c-syntax/issues/162

--- src/extension.ts
+++ src/extension.ts
@@ -25,6 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
     global.getConfiguration     = vscAdapter.getConfiguration;
     global.getConfigurationKey  = vscAdapter.getConfigurationKey;
     global.getRootPath          = vscAdapter.getRootPath;
+    global.fullNameRecursor     = vscAdapter.fullNameRecursor;
     
     context.subscriptions.push(vscode.languages.registerCompletionItemProvider(BSL_MODE, new CompletionItemProvider(global), ".", "="));
     context.subscriptions.push(vscode.languages.registerDefinitionProvider(BSL_MODE, new DefinitionProvider(global)));
--- src/vscAdapter.ts
+++ src/vscAdapter.ts
@@ -20,3 +20,44 @@ export function getConfigurationKey(configuration:vscode.WorkspaceConfiguration,
 export function getRootPath() {
     return vscode.workspace.rootPath;
 }
+
+export function fullNameRecursor(word: string, document: vscode.TextDocument, range: vscode.Range, left: boolean) {
+        let result: string;
+        let plus: number = 1;
+        let newRange: vscode.Range;
+        if (left) {
+            plus = -1;
+            if (range.start.character === 0) {
+                return word;
+            }
+            newRange = new vscode.Range(new vscode.Position(range.start.line, range.end.character - word.length + plus), new vscode.Position(range.start.line, range.start.character));
+        } else {
+            newRange = new vscode.Range(new vscode.Position(range.end.line, range.end.character), new vscode.Position(range.end.line, range.end.character + plus));
+        }
+        let dot = document.getText(newRange);
+        if (dot.endsWith(".")) {
+            let newPosition: vscode.Position;
+            if (left) {
+                let leftWordRange: vscode.Range = document.getWordRangeAtPosition(newRange.start);
+                if (leftWordRange !== undefined) {
+                    result = document.getText(leftWordRange) + "." + word;
+                    if (leftWordRange !== undefined && leftWordRange.start.character > 1) {
+                        newPosition = new vscode.Position(leftWordRange.start.line, leftWordRange.start.character - 1);
+                    } else {
+                        newPosition = new vscode.Position(leftWordRange.start.line, 0);
+                    }
+                }
+            } else {
+                result = word + "." + document.getText(document.getWordRangeAtPosition(newRange.start));
+                newPosition = new vscode.Position(newRange.end.line, newRange.end.character + 2);
+            }
+            if (document.getText(new vscode.Range(new vscode.Position(newPosition.line, newPosition.character + 1), newPosition)) === ".") {
+                let newWord = document.getWordRangeAtPosition(newPosition);
+                return document.getText(newWord) +  "." + result;
+            }
+            return result;
+        } else {
+            result = word;
+            return result;
+        }
+    }
