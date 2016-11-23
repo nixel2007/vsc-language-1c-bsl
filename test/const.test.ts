759632b3e4fa418ea32dca40a595d70d07796276
Nikita Gryzlov <na_gryzlov@guu.ru>
2016-11-23 10:57:25 +0700

    Добавлена мока как тестовый фреймворк и первый тест

---
+++ test/const.test.ts
@@ -0,0 +1,16 @@
+// The module 'assert' provides assertion methods from node
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+import {BSL_MODE} from '../src/const';
+
+// Defines a Mocha test suite to group tests of similar kind together
+suite("Const Tests", () => {
+
+    // Defines a Mocha unit test
+    test("Filter is correct", () => {
+        assert.equal(BSL_MODE, "bsl");
+    });
+});
