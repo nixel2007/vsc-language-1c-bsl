b863dc23710b6d343d8148d87da63df9f56904ca
Nikita Gryzlov <nixel2007@gmail.com>
2016-12-16 18:13:30 +0300

    Включение сонар-сканнера в травис

---
+++ travis.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+npm test --silent
+
+function jsonval {
+    temp=`cat $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop | cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g' `
+    version=${temp##*|}
+}
+
+json=package.json
+prop='version'
+
+jsonval
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  sonar-scanner \
+      -Dsonar.host.url=http://sonar,silverbulleters.org \
+      -Dsonar.analysis.mode=preview \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.login=$SONAR_OAUTH
+
+elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  sonar-scanner \
+      -Dsonar.host.url=http://sonar.silverbulleters.org \
+      -Dsonar.login=$SONAR_OAUTH \
+      -Dsonar.projectVersion=$version
+fi
