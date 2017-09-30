6b1497e8324522bfeb75c0b727714652fbd1108f
bambr1975 <bambr1975@yandex.ru>
2017-09-30 09:23:49 +0300

    Устранение дублирования

--- src/features/signatureHelpProvider.ts
+++ src/features/signatureHelpProvider.ts
@@ -2,6 +2,7 @@
 import {CancellationToken, Position, SignatureHelp, SignatureHelpProvider,
     SignatureInformation, TextDocument} from "vscode";
 import AbstractProvider from "./abstractProvider";
+import BackwardIterator from "./backwardIterator";
 
 const _NL = "\n".charCodeAt(0);
 const _TAB = "\t".charCodeAt(0);
@@ -24,44 +25,6 @@ const _0 = "0".charCodeAt(0);
 const _9 = "9".charCodeAt(0);
 const _Dot = ".".charCodeAt(0);
 
-const BOF = 0;
-
-class BackwardIterator {
-    private lineNumber: number;
-    private offset: number;
-    private line: string;
-    private model: TextDocument;
-
-    constructor(model: TextDocument, offset: number, lineNumber: number) {
-        this.lineNumber = lineNumber;
-        this.offset = offset;
-        this.line = model.lineAt(this.lineNumber).text;
-        this.model = model;
-    }
-
-    public hasNext(): boolean {
-        return this.lineNumber >= 0;
-    }
-
-    public next(): number {
-        if (this.offset < 0) {
-            if (this.lineNumber > 0) {
-                this.lineNumber--;
-                this.line = this.model.lineAt(this.lineNumber).text;
-                this.offset = this.line.length - 1;
-                return _NL;
-            }
-            this.lineNumber = -1;
-            return BOF;
-        }
-        const ch = this.line.charCodeAt(this.offset);
-        this.offset--;
-        return ch;
-    }
-
-}
-
-// tslint:disable-next-line:max-classes-per-file
 export default class GlobalSignatureHelpProvider extends AbstractProvider implements SignatureHelpProvider {
 
     public provideSignatureHelp(document: TextDocument,
